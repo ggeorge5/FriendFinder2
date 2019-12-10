5b82335db5ded0751e5c58f10566c7bee3c57c98
ggeorge <gmgeorge1993@gmail.com>
2019-12-09 21:12:37 -0600

    added all the content

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+      name: "James Franco",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi4lcaV76nmAhWeFjQIHdFzDJoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.hollywoodreporter.com%2Fthr-esq%2Fjames-franco-sued-sexual-harassment-by-acting-students-1245314&psig=AOvVaw3PIlmBt3y_kMF4FGhPLwlk&ust=1576025638645887",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Tom Hardy",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi-nans76nmAhWU4J4KHU3cC_4QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.biography.com%2Factor%2Ftom-hardy&psig=AOvVaw10kyGe0bLSuFRJ2JTh53Gu&ust=1576025820546892",
+        scores: [4, 3, 2, 5, 3, 3, 1, 2, 1, 2]
+    },
+    {
+        name: "Jimmy Fallon",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi7qNSB8KnmAhUIrZ4KHfJtAycQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.wbur.org%2Fhereandnow%2F2019%2F10%2F10%2Fjimmy-fallon-this-is-baby&psig=AOvVaw3VveB2y8tzNXrknFmZsfds&ust=1576025863116782",
+        scores: [5, 2, 3, 3, 4, 3, 2, 4, 5, 3]
+    },
+    {
+        name: "Seth Rogan",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwielayT8KnmAhWgJTQIHR0wCysQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.imdb.com%2Fname%2Fnm0736622%2F&psig=AOvVaw3TgAEsNMXmShd7NT_4CNwf&ust=1576025903252119",
+        scores: [4, 1, 2, 3, 3, 2, 5, 5, 1, 3]
+    },
+    {
+        name: "Matthew Mcconaughey",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiyjZmu8KnmAhX3IjQIHfx8CKoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.britannica.com%2Fbiography%2FMatthew-McConaughey&psig=AOvVaw2W034wsTpvyT_Y_X0r7nxT&ust=1576025947787004",
+        scores: [3, 5, 1, 2, 3, 4, 5, 2, 3, 4]
+    }
+];
+
+module.exports = friends;
